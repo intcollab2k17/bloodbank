1d0921a5ab781508a4063d2f5dd4321c5ccd3d33
intcollab2k17 <intcollab2k17@gmail.com>
2017-08-06 13:46:37 +0800

    sidebar update

---
+++ user/admin/add_nationality_modal.php
@@ -0,0 +1,24 @@
+<div class="modal fade" id="uiModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                            <h4 class="modal-title" id="H2">Modal Form</h4>
+                                        </div>
+                                        <div class="modal-body">
+                                        <form method = "POST" action = "add_user.php" enctype ="multipart/form-data">
+												<div class="form-group">
+													<label>Username</label>
+													<input class="form-control" name = "username" Placeholder = "Create a username.." required = "true"/>
+													<p class="help-block">This username accoun is used for your log in </p>
+												</div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                            <button name = "save_user" class="btn btn-primary">Save changes</button>
+                                        </div>
+									</form>
+                                    </div>
+                                </div>
+                     </div>
+                   
