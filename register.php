da4d4f6662372e43c4dc7cf5b733c2de5fb86d0e
intcollab2k17 <intcollab2k17@gmail.com>
2017-08-06 20:46:21 +0800

    add register.php

--- register.php
+++ register.php
@@ -19,8 +19,7 @@
 		include('includes/dbcon.php');
 	      
 		$query=mysqli_query($con,"SELECT * from donor where donor_first='$first' and donor_last='$last' and donor_bday='$bday'");
-
-			$num_rows = mysqli_num_rows($query);
+				$num_rows = mysqli_num_rows($query);
 				while($row=mysqli_fetch_array($query)){
 			  	$id=$row['donor_id'];}
 
@@ -30,11 +29,45 @@
 						echo "<script>document.location='login.html';</script>";
 
  				}
+
 		else
 				{
-
 					mysqli_query($con,"INSERT INTO donor(donor_first,donor_middle,donor_last,donor_email,donor_password,donor_nationality,donor_occupation,donor_address,donor_city,donor_province,donor_contact,donor_tel,donor_gender,donor_bday,donor_pic) 
-		VALUES('$first','$mi','$last','$email','$password','$nationality','$occupation','$address','$city','$province','$contact','$telephone','$gender','$bday','default.gif')")or die(mysqli_error($con));  
+					VALUES('$first','$mi','$last','$email','$password','$nationality','$occupation','$address','$city','$province','$contact','$telephone','$gender','$bday','default.gif')")or die(mysqli_error($con));
+
+							$to = $email;
+							$subject = "Email Verification";
+
+							$message = "
+							<html>
+							<head>
+							<title>Email Verification for ".$email."</title>
+							</head>
+							<body>
+							<p>This email contains Email !</p>
+							<table>
+							<tr>
+							<th>".$first. ' '.$last."</th>
+							<th>Email Verification</th>
+							</tr>
+							<tr>
+							<td><a></a></td>
+							<td></td>
+							</tr>
+							</table>
+							</body>
+							</html>
+							";
+
+							// Always set content-type when sending HTML email
+							$headers = "MIME-Version: 1.0" . "\r\n";
+							$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+							// More headers
+							$headers .= 'From: <webmaster@example.com>' . "\r\n";
+							$headers .= 'Cc: myboss@example.com' . "\r\n";
+
+							mail($to,$subject,$message,$headers);
 	
 						echo "<script type='text/javascript'>alert('Successfully registered as a donor in NIR Blood Bank! You may now login!');</script>";
 						echo "<script>document.location='login.html';</script>";
