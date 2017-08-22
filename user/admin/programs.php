9b7a6ede0ca517d1c671f2367eba151add3cb3ef
intcollab2k17 <intcollab2k17@gmail.com>
2017-08-23 01:18:44 +0800

    Final Push of the night

--- user/admin/programs.php
+++ user/admin/programs.php
@@ -64,6 +64,53 @@
 												<a href="#update<?php echo $id;?>" class="btn btn-success" data-toggle = "modal" data-target="#update<?php echo $id;?>"><i class = "fa fa-pencil"></i> Edit</a>
 											</td>
                                         </tr> 
+                                <div class="modal fade" id="update<?=$id;?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                            <h4 class="modal-title" id="H2">Edit Program</h4>
+                                        </div>
+                                        <div class="modal-body">
+                                        <form method = "POST" action = "edit_program.php" enctype ="multipart/form-data">
+                                                <div class="form-group">
+                                                    <label>Program</label>
+                                                    <input class="form-control" name = "program" value="<?=$row['program'];?>" required = "true"/>  
+                                                      <input class="form-control" name = "program_id" value="<?=$row['program_id'];?>" required = "true"/>                                                
+                                                </div>
+                                                <div class="form-group">
+                                                    <label>Program Address</label>
+                                                    <input class="form-control"  name = "program_address" value="<?=$row['program_address'];?>"  />
+                                                </div>
+                                                <div class="form-group">
+                                                    <label>Program Date</label>
+                                                    <input  class="datepicker form-control"  name = "program_date" value="<?=$row['program_date'];?>" data-date-format="mm-dd-yyyy" />
+                                                </div>
+                                                <div class="form-group">
+                                                    <label>Program Date</label>
+                                                    <input  type = "time" class="timepicker form-control"  name = "program_time" value="<?=$row['program_time'];?>" />
+                                                </div>
+                                    
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                            <button name = "save_user" class="btn btn-primary">Save changes</button>
+                                        </div>
+                                    </form>
+                                    </div>
+                                </div>
+                     </div>
+                   
+
+
+
+
+
+
+
+
+
+
 										<?php }?>									
                                     </tbody>
                                 </table>
@@ -91,6 +138,14 @@
    <?php include 'script.php';?>
         <script>
             $(function () { formInit(); });
+
+
+
+            $('.datepicker').datepicker({
+               minDate: 0,        
+              format: 'yyyy-mm-dd'
+              
+        });
         </script>
 
     <!-- END GLOBAL SCRIPTS -->
