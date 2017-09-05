5f1240b8e24a003efcf1dfc0c9417c76fbe567e6
Kenneth <kennethaboy2k16@gmail.com>
2017-09-05 12:34:22 +0800

    EEW

--- calendar.php
+++ calendar.php
@@ -1,22 +1,8 @@
 <div class="choose-us-block content text-center margin-bottom-40" id="calendar">
-					<div class="container">
-					
-					<h2>Calendar <strong>of Events</strong></h2>
-							<div  class = "porlet-body" style = "margin-left:200px;">
-							<?php
-							include_once('functions.php');
-							echo getCalender(); ?>
-								</div>
-								
-								
-							
-						
-				
-					</div>
-					</div>
-
-
+	<div class="container">
+		<h2>Calendar <strong>of Events</strong></h2>
+		<div  class = "porlet-body" style = "margin-left:200px;">
 		
-		
-		
-		
+		</div>
+	</div>
+</div>
